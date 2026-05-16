c4736df7063280f2dfe8b0a6174e2e825d6e1f77
brysett-valenzuela <brysett.valenzuela@ucsp.edu.pe>
2026-05-16 18:28:57 -0500

    Update valenzuela.sql

--- valenzuela.sql
+++ valenzuela.sql
@@ -27,3 +27,13 @@ CREATE TABLE infraccion (
     pnlzcon_ecnmca NUMERIC(10, 2),
     CONSTRAINT pk_infraccion PRIMARY KEY (id_infrccon)
 );
+CREATE TABLE dtlle_pddo (
+    id_pedido INTEGER NOT NULL,
+    id_prdcto INTEGER NOT NULL,
+    unidades INTEGER NOT NULL,
+    precio_unidad NUMERIC(10, 2) NOT NULL,
+    precio_final NUMERIC(10, 2) NOT NULL,
+    CONSTRAINT pk_dtlle_pddo PRIMARY KEY (id_pedido, id_prdcto),
+    CONSTRAINT fk_dtlle_pddo_pedido FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido),
+    CONSTRAINT fk_dtlle_pddo_producto FOREIGN KEY (id_prdcto) REFERENCES producto(id_prdcto)
+);
