4498c35c1e9e55ecf788dd5778a026894e680443
Mage <brysett.valenzuela@ucsp.edu.pe>
2026-05-21 09:25:55 -0500

    datos_insertados

--- insert.sql
+++ insert.sql
@@ -1,1 +1,17 @@
+-- Datos insertados --
+INSERT INTO lugar (distrito, provincia, region) VALUES
+('Miraflores', 'Lima', 'Lima'),
+('Santiago de Surco', 'Lima', 'Lima'),
+('Yanahuara', 'Arequipa', 'Arequipa'),
+('Wanchaq', 'Cusco', 'Cusco'),
+('Trujillo', 'Trujillo', 'La Libertad'),
+('Chiclayo', 'Chiclayo', 'Lambayeque'),
+('Piura', 'Piura', 'Piura'),
+('Huancayo', 'Huancayo', 'Junín');
 
+INSERT INTO turno (nmbre_trno, hora_inicio, hora_fin) VALUES
+('Mañana', '06:00:00', '14:00:00'),
+('Tarde', '14:00:00', '22:00:00'),
+('Noche', '22:00:00', '06:00:00'),
+('Oficina', '08:00:00', '17:00:00'),
+('Partidario', '09:00:00', '13:00:00');
